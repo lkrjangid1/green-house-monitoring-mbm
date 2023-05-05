d9f961d5c67a41be4941071f86d136c09fef03eb
lkrjangid1 <lkrjangid1@gmail.com>
2023-05-05 07:30:11 +0530

    remove bugs

--- major_project.ino
+++ major_project.ino
@@ -6,6 +6,8 @@
 #include "addons/RTDBHelper.h"
 #include <NTPClient.h>
 #include <WiFiUdp.h>
+#include <Servo.h>  // servo library
+
 
 // Define NTP Client to get time
 WiFiUDP ntpUDP;
@@ -28,115 +30,172 @@ FirebaseConfig config;
 #define API_KEY "AIzaSyAwREwEc71sAk0TV1TT0nL8-wLHDSLT4G4"
 #define DATABASE_URL "https://green-house-monitoring-mbm-default-rtdb.asia-southeast1.firebasedatabase.app/"
 
-#define WIFI_SSID "Error 404"
-#define WIFI_PASSWORD "helloworld"
+#define WIFI_SSID "Infinix HOT 7"
+#define WIFI_PASSWORD "lkrjangid99"
 
 #define USER_EMAIL "lkrjangid@gmail.com"
 #define USER_PASSWORD "lkrjangid@123"
 
 std::string databasePath;
 
-int soilPin = 16, // D0
-    lightPin = 3; // RX
-int relay_fan_1 = 12, // D6
-    relay_fan_2 = 13, // D7
-    relay_led = 14, // D5
-    relay_pump = 15; // D8
+int soilPin = 16,      // D0
+  lightPin = 5;        // D1
+int relay_fan_1 = 12,  // D6
+  relay_led = 14,      // D5
+  relay_pump = 13,     // D7
+  humidity_refrance_value = 60,
+    temprature_refrance_value = 25,
+    is_operate_manually = 0,
+    is_turn_on_LED = 0,
+    is_turn_on_servo = 0,
+    is_turn_on_fan_1 = 0,
+    is_turn_on_pump = 0;
 
-DHT dht2(2, DHT11); // D4
+DHT dht2(0, DHT11);  // D4
+Servo s1;
 
 bool signupOK = false;
 
 void setup() {
+  s1.attach(4);  // servo attach D2 pin of arduino
   pinMode(soilPin, INPUT);
   pinMode(lightPin, INPUT);
   pinMode(relay_fan_1, OUTPUT);
-  pinMode(relay_fan_2, OUTPUT);
   pinMode(relay_led, OUTPUT);
   pinMode(relay_pump, OUTPUT);
   dht2.begin();
   Serial.begin(9600);
-//   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-//   while (WiFi.status() != WL_CONNECTED) {
-//     delay(250);
-//     Serial.print(".");
-//   }
-//   Serial.println("");
-//   Serial.println("WiFi Connected!");
-//   Serial.println(WiFi.localIP());
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(250);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi Connected!");
+  Serial.println(WiFi.localIP());
 
-//   config.api_key = API_KEY;
-//   config.database_url = DATABASE_URL;
+  config.api_key = API_KEY;
+  config.database_url = DATABASE_URL;
 
-//   auth.user.email = USER_EMAIL;
-//   auth.user.password = USER_PASSWORD;
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
 
-//   if (Firebase.signUp(&config, &auth, "", "")) {
-//     Serial.println("ok");
-//     signupOK = true;
-//   } else {
-//     Serial.printf("%s\n", config.signer.signupError.message.c_str());
-//   }
-//   config.token_status_callback = tokenStatusCallback;  //see addons/TokenHelper.h
+  if (Firebase.signUp(&config, &auth, "", "")) {
+    Serial.println("ok");
+    signupOK = true;
+  } else {
+    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+  }
+  config.token_status_callback = tokenStatusCallback;  //see addons/TokenHelper.h
 
-// #if defined(ESP8266)
-//     // In ESP8266 required for BearSSL rx/tx buffer for large data handle, increase Rx size as needed.
-//   fbdo.setBSSLBufferSize(2048 /* Rx buffer size in bytes from 512 - 16384 */, 2048 /* Tx buffer size in bytes from 512 - 16384 */);
-// #endif
-//   // Limit the size of response payload to be collected in FirebaseData
-//   fbdo.setResponseSize(2048);
-//   Firebase.begin(&config, &auth);
+#if defined(ESP8266)
+    // In ESP8266 required for BearSSL rx/tx buffer for large data handle, increase Rx size as needed.
+  fbdo.setBSSLBufferSize(2048 /* Rx buffer size in bytes from 512 - 16384 */, 2048 /* Tx buffer size in bytes from 512 - 16384 */);
+#endif
+  // Limit the size of response payload to be collected in FirebaseData
+  fbdo.setResponseSize(2048);
+  Firebase.begin(&config, &auth);
 
-//   Serial.println("Firebase Config");
-//   timeClient.begin();
+  Serial.println("Firebase Config");
+  timeClient.begin();
 }
 
 void loop() {
-  // epochTime = getTime();
-  // databasePath = "ghm_data/" + std::to_string(epochTime);
-  // int soilData = digitalRead(soilPin);  //Soil moisture threshold
-  // if (soilData == 1) {
-  //   digitalWrite(relay_pump, LOW);
-  // } else if (soilData == 0) {
-  //   digitalWrite(relay_pump, HIGH);
-  // }
-
+  epochTime = getTime();
+  databasePath = "ghm_data/" + std::to_string(epochTime);
+  getFirebaseData();
+  int soilData = digitalRead(soilPin);    //Soil moisture threshold
   int lightData = digitalRead(lightPin);  //Light intensity
-  Serial.println("light");
-  Serial.println(lightData);
-  // if (lightData == 1) {
-  //   digitalWrite(relay_led, LOW);
-  // } else if (lightData == 0) {
-  //   digitalWrite(relay_led, HIGH);
-  // }
+  float t = dht2.readTemperature();
+  float h = dht2.readHumidity();
 
-  // float t = dht2.readTemperature();
-  // if (t > 35) {
-  //   digitalWrite(relay_fan_1, LOW);
-  // } else if (t < 35) {
-  //   digitalWrite(relay_fan_1, HIGH);
-  // }
+  if (is_operate_manually == 1) {
+    manualOperate();
+  } else {
+    autoOperate(soilData, lightData, t, h);
+  }
 
-  // float h = dht2.readHumidity();
-  // if (h > 60) {
-  //   digitalWrite(relay_fan_2, LOW);
-  // } else if (h < 60) {
-  //   digitalWrite(relay_fan_2, HIGH);
-  // }
-
-  // if (Firebase.ready()) {
-  //   // to store old data
-  //   firebaseRTDBFunc(databasePath, t, h, lightData, soilData);
-
-  //   // to work in real time
-  //   firebaseRTDBFunc("ghm_real_time_data", t, h, lightData, soilData);
-
-    delay(1000);
-  // } else {
-  //   Serial.println("Error in connecting");
-  // }
+  if (Firebase.ready()) {
+    // to store old data
+    firebaseRTDBFunc(databasePath, t, h, lightData, soilData);
+    // to work in real time
+    firebaseRTDBFunc("ghm_real_time_data", t, h, lightData, soilData);
+    delay(5000);
+  } else {
+    Serial.println("Error in connecting");
+  }
 }
 
+void getFirebaseData() {
+  Firebase.RTDB.getInt(&fbdo, "ghm_real_time_data/humidity_refrance_value");
+  humidity_refrance_value = fbdo.to<int>();
+  Firebase.RTDB.getInt(&fbdo, "ghm_real_time_data/temprature_refrance_value");
+  temprature_refrance_value = fbdo.to<int>();
+  Firebase.RTDB.getInt(&fbdo, "ghm_real_time_data/is_operate_manually");
+  is_operate_manually = fbdo.to<int>();
+  Firebase.RTDB.getInt(&fbdo, "ghm_real_time_data/is_turn_on_pump");
+  is_turn_on_pump = fbdo.to<int>();
+  Firebase.RTDB.getInt(&fbdo, "ghm_real_time_data/is_turn_on_fan_1");
+  is_turn_on_fan_1 = fbdo.to<int>();
+  Firebase.RTDB.getInt(&fbdo, "ghm_real_time_data/is_turn_on_servo");
+  is_turn_on_servo = fbdo.to<int>();
+  Firebase.RTDB.getInt(&fbdo, "ghm_real_time_data/is_turn_on_LED");
+  is_turn_on_LED = fbdo.to<int>();
+}
+
+void manualOperate() {
+  if (is_turn_on_pump == 1) {
+    digitalWrite(relay_pump, HIGH);
+  } else {
+    digitalWrite(relay_pump, LOW);
+  }
+  if (is_turn_on_fan_1 == 1) {
+    digitalWrite(relay_fan_1, HIGH);
+  } else {
+    digitalWrite(relay_fan_1, LOW);
+  }
+  if (is_turn_on_servo == 1) {
+    s1.write(90);
+  } else {
+    s1.write(0);
+  }
+  if (is_turn_on_LED == 1) {
+    digitalWrite(relay_led, HIGH);
+  } else {
+    digitalWrite(relay_led, LOW);
+  }
+}
+
+void autoOperate(int soilData, int lightData, float t, float h) {
+  if (soilData == 1) {
+    digitalWrite(relay_pump, HIGH);
+    sendFCMNotification("Warning water lavel is low", "It's time to irrigation");
+  } else {
+    digitalWrite(relay_pump, LOW);
+  }
+
+  if (lightData == 1) {
+    digitalWrite(relay_led, LOW);
+    sendFCMNotification("Warning", "It's time turn on the light");
+  } else {
+    digitalWrite(relay_led, HIGH);
+  }
+
+  if (t > temprature_refrance_value) {
+    digitalWrite(relay_fan_1, LOW);
+    digitalWrite(relay_led, LOW);
+    sendFCMNotification("Warning high temperature", "It's time turn on the fan");
+  } else {
+    digitalWrite(relay_fan_1, HIGH);
+  }
+
+  if (h > humidity_refrance_value) {
+    sendFCMNotification("Warning high humidity", "It's time to open the window");
+    s1.write(90);
+  } else {
+    s1.write(0);
+  }
+}
 
 void firebaseRTDBFunc(std::string databasePath, int temperature, int humidity, float lightData, float soilData) {
   if (Firebase.RTDB.setString(&fbdo, databasePath + "/timestamp", std::to_string(epochTime))) {
